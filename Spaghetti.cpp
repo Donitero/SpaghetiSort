38550ea8a196309ab5a7500bedb9fe9aaba6f685
Donitero <159440525+Donitero@users.noreply.github.com>
2026-03-15 23:15:00 +0300

    Add spaghetti sort function with comparison tracking
    Implement spaghetti sort algorithm with comparison and wrap counting.

--- Spaghetti.cpp
+++ Spaghetti.cpp
@@ -2,4 +2,26 @@
 #include<vector>
 using namespace std;
 
+void spaghettiSort(vector<int>& arr, int& comparisons, int&wraps){
+  comparisons = 0;
+wraps = 0;
+int n = arr.size();
+
+for (int i = 0, i < n-1, ++i){
+  int maxIdx = i;
+  for(int j = i + 1, j < n, ++j){
+  comparison++;
+  if (arr[j] > arr[maxIdx]){
+  maxIdx = j;
+  }
+  }
+if (maxIdx != i){
+  int temp = arr[maxIdx];
+  arr[maxIdx] = arr[i];
+  arr[i] = temp;
+}
+}
+}
+  
+
 
