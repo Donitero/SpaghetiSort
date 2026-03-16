76895f857064fe4eeee3ac6db1ae2a05472829f1
Donitero <159440525+Donitero@users.noreply.github.com>
2026-03-16 14:19:53 +0300

    Add spaghetti sort function with counters
    Implemented spaghetti sort algorithm with comparison and swap counters.

--- Spaghetti.cpp
+++ Spaghetti.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
 
+void spaghettiSort(vector<int>& arr, int& comparisons, int& swaps){
+  comparisons = 0;
+  swaps = 0;
+  int n = arr.size();
+
+  for(int i = 0; i < n-1; ++i){
+    int maxIdx = i;
+    for(int j = i+1; j < n; ++j){
+      comparisons++;
+      if(arr[j] > arr[maxIdx]){
+        maxIdx = j;
+      }
+    }
+    if(maxIdx != i){
+      int temp = arr[maxIdx];
+      arr[maxIdx] = arr[i];
+      arr[i] = temp;
+    }
+  }
+}
